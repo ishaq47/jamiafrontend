b9d7d52bb224d7eb2cf74ec2fe592aa12c3e0f54
IshaqFrontendDev <muhammad.ishaq@simplexmed.com>
2026-04-23 19:40:04 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,30 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
-        VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-       name: 'Jamia Uloom Islamia',
-        short_name: 'Jamia',
-        description: 'Jamia Uloom Islamia Official App',
-         theme_color: '#0B6B3A',
-        background_color: '#FFFFFF',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: '/logo1.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/logo.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+  plugins: [react(), 
+    tailwindcss(),
+      VitePWA()
   ],
 })
