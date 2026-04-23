dc0d1ad8d7cfacccbe640d924f81d9967c7027d8
IshaqFrontendDev <muhammad.ishaq@simplexmed.com>
2026-04-23 19:42:24 +0500

    code updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), 
-    tailwindcss(),
-      VitePWA()
-  ],
+  plugins: [react(), tailwindcss()],
 })
