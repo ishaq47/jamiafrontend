0c82fa545d6e74addfce4956f76d6f5595dd3f51
IshaqFrontendDev <muhammad.ishaq@simplexmed.com>
2026-04-24 12:16:38 +0500

    icon for admin panel removed

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
-        VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-       name: 'Jamia Uloom Islamia',
-        short_name: 'Jamia',
-        description: 'Jamia Uloom Islamia Official App',
-         theme_color: '#0B6B3A',
-        background_color: '#FFFFFF',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: '/logo1.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/logo.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react(), tailwindcss()],
 })
